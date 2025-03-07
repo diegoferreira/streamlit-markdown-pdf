4f3ddf8fe6cfc460a7bb29c49aa2fb05781805e7
Diego Ferreira <tecnodiego@gmail.com>
2025-03-07 13:01:02 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ def convert_markdown_to_pdf(md_text):
 
     # Salvar o PDF em um buffer
     pdf_buffer = BytesIO()
-    pdf.output(pdf_buffer)
+    pdf.output(pdf_buffer, 'F')
     pdf_buffer.seek(0)
     return pdf_buffer
 
